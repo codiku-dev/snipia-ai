46122a32b860fcd68a4a122d9355ce0f674cfd25
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-01-02 23:29:11 +0100

    wip

--- app/(pages)/(auth)/layout.tsx
+++ app/(pages)/(auth)/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout({
   return (
     <div className="flex items-center justify-between h-screen w-screen ">
       <div className="flex-1 flex justify-center items-center">{children}</div>
-      <div className="hidden md:flex space-y-4 flex-1  flex-col items-center bg-main-700 h-full justify-center rounded-bl-[8rem] shadow-2xl">
+      <div className="hidden lg:flex space-y-4 flex-1  flex-col items-center bg-main-700 h-full justify-center rounded-bl-[8rem] shadow-2xl">
         <Image src={logoAuthPng} alt="Logo" />
         <div className="text-2xl text-white">
           The one place for all your code snippets, with a touch of AI
